c9834d5a35229d57bb81a8067a4f2fad398e2189
Junior Marques <jrmarqueshd@gmail.com>
2023-04-03 23:44:58 -0300

    create auto cleaner for breadcrumbs

--- src/contexts/header/index.tsx
+++ src/contexts/header/index.tsx
@@ -1,4 +1,6 @@
 import React, { useState } from 'react';
+import { useLocation } from 'react-router-dom';
+import useDidMount from '../../hooks/useDidMount';
 import { BreadcrumbType, HeaderContextProps } from './interfaces';
 
 const HeaderContext = React.createContext<HeaderContextProps>(null!);
@@ -10,7 +12,13 @@ export function useHeader() {
 export function HeaderProvider({ children }: { children: React.ReactNode }) {
 	const [breadcrumbs, setBreadcrumbs] = useState<BreadcrumbType[]>([]);
 
+	const location = useLocation();
+
 	const value = { breadcrumbs, setBreadcrumbs };
+
+	useDidMount(() => {
+		setBreadcrumbs([]);
+	}, [location.pathname]);
 
 	return <HeaderContext.Provider value={value}>{children}</HeaderContext.Provider>;
 }
